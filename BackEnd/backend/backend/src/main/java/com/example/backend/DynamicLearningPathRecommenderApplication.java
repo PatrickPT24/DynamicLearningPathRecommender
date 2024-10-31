94d4224b17ea73f154597d02736f7b939737d873
PatrickPT24 <161574851+PatrickPT24@users.noreply.github.com>
2024-11-01 04:21:50 +0530

    Structure_Setup_02
    Initial Code_Base_Structure Setup to set the Directories for BackEnd

---
+++ BackEnd/backend/backend/src/main/java/com/example/backend/DynamicLearningPathRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicLearningPathRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicLearningPathRecommenderApplication.class, args);
+	}
+
+}
